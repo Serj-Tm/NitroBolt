618f3e3b36d98768a004e1669d43a1acccf020a2
Serj-Tm <serj-tm@ya.ru>
2015-08-24 01:06:37 +0300

    Bound

---
+++ NitroBolt.Functional/MathHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NitroBolt.Functional
+{
+  public static class MathHelper
+  {
+    public static TItem Bound<TItem>(this TItem value, TItem? minValue, TItem? maxValue) where TItem:struct, IComparable
+    {
+      if (minValue != null && value.CompareTo(minValue) < 0)
+        return minValue.Value;
+      if (maxValue != null && value.CompareTo(maxValue) > 0)
+        return maxValue.Value;
+      return value;
+    }
+  }
+}

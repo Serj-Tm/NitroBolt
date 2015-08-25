db5394bf446e8618ae188f209a536f628307c877
Serj-Tm <serj-tm@ya.ru>
2015-08-25 14:49:53 +0300

    ElementAtRandomOrDefault

---
+++ NitroBolt.Functional/RandomHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NitroBolt.Functional.Random
+{
+    public static class RandomHelper
+    {
+        public static T ElementAtRandomOrDefault<T>(this IList<T> items, System.Random random)
+        {
+            if (items == null || items.Count == 0)
+                return default(T);
+            return items[random.Next(items.Count)];
+        }
+    }
+}

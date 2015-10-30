c3d1d14b4642155be972c1bc99710904a3e898c0
serj-tm <serj-tm@ya.ru>
2015-10-30 13:48:04 +0300

    QItem

---
+++ NitroBolt.Immutable/QItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NitroBolt.Immutable
+{
+    public class QItem
+    {
+        public QItem(ImmutableDictionary<string, object> data = null)
+        {
+            this.Data = data ?? ImmutableDictionary<string, object>.Empty;
+        }
+
+        public readonly ImmutableDictionary<string, object> Data;
+    }
+}
